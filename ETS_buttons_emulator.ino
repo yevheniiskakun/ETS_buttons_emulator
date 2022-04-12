90612f744fedc73ffaf6a82c09eab08677cc950e
yevhinator <yevhenii.skakun@gmail.com>
2022-04-12 22:13:50 +0200

    Added test button press

--- ETS_buttons_emulator.ino
+++ ETS_buttons_emulator.ino
@@ -8,27 +8,25 @@ int button8State = 0;         // variable for reading the pushbutton status
 
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin8, INPUT);
   Keyboard.begin();
 }
 void loop() {
   // read the state of the pushbutton value:
-  button8State = !digitalRead(buttonPin8);
-  // Show the state of pushbutton on serial monitor
-  Serial.println(button8State);
-  // check if the pushbutton is pressed.
   
+  button8State = digitalRead(buttonPin8);
   // if it is, the buttonState is HIGH:
   if (button8State == HIGH) {
     // turn LED on:
-    Serial.println("button 8 on");
-    Keyboard.press('n');
-    delay(100);
+    //Serial.println("button 8 on");
+    Keyboard.press('E');
     Keyboard.releaseAll();
+    button8State = LOW;
+    delay(500);
+    
   }
  
   // Added the delay so that we can see the output of button
-  delay(200);
 }
