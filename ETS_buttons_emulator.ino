808eaa785136dd8613704cf4a656e229b19c5942
yevhinator <yevhenii.skakun@gmail.com>
2022-04-14 22:26:11 +0200

    Added stopwatch to always on display

--- ETS_buttons_emulator.ino
+++ ETS_buttons_emulator.ino
@@ -2,11 +2,21 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
+// ----------Settings----------- 
+int long_start = 0;
+
+const int buttonPin8 = 8;
+// ------------------------------
 unsigned long actual_time = 0;
 unsigned long first_timer = 0;
 unsigned long second_timer = 0;
 
+
 int engine_status_flag = 0;
+
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
@@ -21,7 +31,19 @@ byte Heart[8] = {
 0b00000
 };
 
-const int buttonPin8 = 8;     // the number of the pushbutton pin
+byte Filled[8] =
+{
+0b11111,
+0b11111,
+0b11111,
+0b11111,
+0b11111,
+0b11111,
+0b11111,
+0b11111
+};
+
+     // the number of the pushbutton pin
 
 // variables will change:
 int button8State = 0;         // variable for reading the pushbutton status
@@ -68,21 +90,9 @@ void loop() {
     delay(2000);
     lcd.clear(); 
   }
-  /*
   
+  always_on_display();
   
-  // if it is, the buttonState is HIGH:
-  if (button8State == HIGH) {
-    // turn LED on:
-    //Serial.println("button 8 on");
-    Keyboard.press('E');
-    Keyboard.releaseAll();
-    button8State = LOW;
-    delay(500);
-    
-  }
- */
-
 }
 
 void greeting(){
@@ -94,19 +104,21 @@ void greeting(){
   lcd.write(byte(0));
   delay(2000);
   lcd.clear(); 
-
-  lcd.setCursor(2,0);
-  lcd.print("Please wait");
-  for(int i = 0; i <= 16; i++){
-    lcd.setCursor(i,1);
-    lcd.print("-");
-    delay(500);
+  
+  if(long_start){
+    lcd.setCursor(2,0);
+    lcd.print("Please wait");
+    for(int i = 0; i <= 16; i++){
+      lcd.setCursor(i,1);
+      lcd.print("-");
+      delay(500);
+    }
+    lcd.clear(); 
+    lcd.setCursor(5,0);
+    lcd.print("Done");
+    delay(2000);
+    lcd.clear(); 
   }
-  lcd.clear(); 
-  lcd.setCursor(5,0);
-  lcd.print("Done");
-  delay(2000);
-  lcd.clear(); 
   lcd.setCursor(1,0);
   lcd.print("Enjoy the ride");
   delay(3000);
@@ -114,6 +126,31 @@ void greeting(){
 }
 
 void always_on_display(){
-
+  //stopwatch();
+  lcd.clear();
+}
+/*
+void stopwatch(){
+  if (actual_time - first_timer >= 1000UL) {
+    //Zapamietaj aktualny czas
+    first_timer = actual_time;
+    
+    if (seconds > 59){
+      minutes++;
+      seconds = 0;
+      lcd.clear();
+    }
+    if(minutes == 60){
+      hours++;
+      minutes = 0;
+      lcd.clear();
+    }
+    seconds++;
+    lcd.setCursor(1,0);
+    lcd.print(hours);
+    lcd.print(":");
+    lcd.print(minutes);
+  }
   
 }
+*/
