5b8b1a378c4f42d96668056e8a66b3cd9f20003e
yevheniiskakun <56674669+yevheniiskakun@users.noreply.github.com>
2022-08-04 23:36:01 +0200

    Added innitial code

--- ETS_buttons_emulator.ino
+++ ETS_buttons_emulator.ino
@@ -1,169 +1,168 @@
-#include <Keyboard.h>
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+#include <Joystick.h>
 
-// ----------Settings----------- 
-int long_start = 0;
+//DEFINITIONS
+#define ENABLE_PULLUPS
+#define NUMROTARIES 0 //replace "?" with number of rotary encoders you are using
+#define NUMBUTTONS 42 //replace "?"with number of buttong you are using
+#define NUMROWS 7 //replace "?" with number of rows you have
+#define NUMCOLS 6 //replace "?" with number of columns you have
 
-const int buttonPin8 = 8;
-// ------------------------------
-unsigned long actual_time = 0;
-unsigned long first_timer = 0;
-unsigned long second_timer = 0;
-
-
-int engine_status_flag = 0;
-
-int hours = 0;
-int minutes = 0;
-int seconds = 0;
-
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
-byte Heart[8] = {
-0b00000,
-0b01010,
-0b11111,
-0b11111,
-0b01110,
-0b00100,
-0b00000,
-0b00000
+//BUTTON MATRIX
+//first change number of rows and columns to match your button matrix, 
+//then replace all "?" with numbers (starting from 0)
+byte buttons[NUMROWS][NUMCOLS] = {
+  {0,1,2,3,4,5},
+  {6,7,8,9,10,11},
+  {12,13,14,15,16,17},
+  {18,19,20,21,22,23},
+  {24,25,26,27,28,29},
+  {30,31,32,33,34,35},
+  {36,37,38,39,40,41}
 };
 
-byte Lightning[8] =
-{
-0b00001,
-0b00011,
-0b00110,
-0b01100,
-0b11110,
-0b00110,
-0b01100,
-0b11000
+struct rotariesdef {
+  byte pin1;
+  byte pin2;
+  int ccwchar;
+  int cwchar;
+  volatile unsigned char state;
 };
 
-     // the number of the pushbutton pin
 
-// variables will change:
-int button8State = 0;         // variable for reading the pushbutton status
+#define DIR_CCW 0x10
+#define DIR_CW 0x20
+#define R_START 0x0
+
+#ifdef HALF_STEP
+#define R_CCW_BEGIN 0x1
+#define R_CW_BEGIN 0x2
+#define R_START_M 0x3
+#define R_CW_BEGIN_M 0x4
+#define R_CCW_BEGIN_M 0x5
+const unsigned char ttable[6][4] = {
+  // R_START (00)
+  {R_START_M,            R_CW_BEGIN,     R_CCW_BEGIN,  R_START},
+  // R_CCW_BEGIN
+  {R_START_M | DIR_CCW, R_START,        R_CCW_BEGIN,  R_START},
+  // R_CW_BEGIN
+  {R_START_M | DIR_CW,  R_CW_BEGIN,     R_START,      R_START},
+  // R_START_M (11)
+  {R_START_M,            R_CCW_BEGIN_M,  R_CW_BEGIN_M, R_START},
+  // R_CW_BEGIN_M
+  {R_START_M,            R_START_M,      R_CW_BEGIN_M, R_START | DIR_CW},
+  // R_CCW_BEGIN_M
+  {R_START_M,            R_CCW_BEGIN_M,  R_START_M,    R_START | DIR_CCW},
+};
+#else
+#define R_CW_FINAL 0x1
+#define R_CW_BEGIN 0x2
+#define R_CW_NEXT 0x3
+#define R_CCW_BEGIN 0x4
+#define R_CCW_FINAL 0x5
+#define R_CCW_NEXT 0x6
+
+const unsigned char ttable[7][4] = {
+  // R_START
+  {R_START,    R_CW_BEGIN,  R_CCW_BEGIN, R_START},
+  // R_CW_FINAL
+  {R_CW_NEXT,  R_START,     R_CW_FINAL,  R_START | DIR_CW},
+  // R_CW_BEGIN
+  {R_CW_NEXT,  R_CW_BEGIN,  R_START,     R_START},
+  // R_CW_NEXT
+  {R_CW_NEXT,  R_CW_BEGIN,  R_CW_FINAL,  R_START},
+  // R_CCW_BEGIN
+  {R_CCW_NEXT, R_START,     R_CCW_BEGIN, R_START},
+  // R_CCW_FINAL
+  {R_CCW_NEXT, R_CCW_FINAL, R_START,     R_START | DIR_CCW},
+  // R_CCW_NEXT
+  {R_CCW_NEXT, R_CCW_FINAL, R_CCW_BEGIN, R_START},
+};
+#endif
+
+//BUTTON MATRIX PART 2
+byte rowPins[NUMROWS] = {1,2,3,4,5,6,7}; //change "?" to the pins the rows of your button matrix are connected to
+byte colPins[NUMCOLS] = {8,9,10,11,12,13}; //change "?" to the pins the rows of your button matrix are connected to
+
+Keypad buttbx = Keypad( makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS);
+
+//JOYSTICK SETTINGS
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
+  JOYSTICK_TYPE_JOYSTICK,
+  42, //number of buttons
+  0, //number of hat switches
+  //Set as many axis to "true" as you have potentiometers for
+  false, // y axis
+  false, // x axis
+  false, // z axis
+  false, // rx axis
+  false, // ry axis
+  false, // rz axis
+  false, // rudder
+  false, // throttle
+  false, // accelerator
+  false, // brake
+  false); // steering wheel
+
+const int numReadings = 20;
+ 
+int readings[numReadings];      // the readings from the analog input
+int index = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int currentOutputLevel = 0;
+
+
+
+               
+
 
 
 void setup() {
-  actual_time = millis();
-  //Serial.begin(9600);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin8, INPUT);
-  Keyboard.begin();
-
-  lcd.init();                      // initialize the lcd 
-  lcd.backlight();                  // turn on the backlight
-  
-  greeting();
+  Joystick.begin();
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
 }
+
 void loop() {
-  actual_time = millis();
-  // Read button state from 8 digital pin
-  button8State = digitalRead(buttonPin8);
-  
-  if (button8State == HIGH && engine_status_flag == 0) {
-    lcd.clear();
-    lcd.setCursor(2,0);
-    lcd.print("Ignition ON");
-    lcd.setCursor(3,1);
-    lcd.print("Engine ON");
-    Keyboard.press('E');    // keyboard press
-    Keyboard.releaseAll();
-    button8State = LOW;
-    engine_status_flag = 1;
-    delay(2000);
-    lcd.clear();   
-  }
-  else if(button8State == HIGH && engine_status_flag == 1){
-    lcd.clear();
-    lcd.setCursor(2,0);
-    lcd.print("Ignition OFF");
-    lcd.setCursor(3,1);
-    lcd.print("Engine OFF");
-    Keyboard.press('E');    // keyboard pressEE
-    Keyboard.releaseAll();
-    button8State = LOW;
-    engine_status_flag = 0;
-    delay(2000);
-    lcd.clear(); 
-  }
-  lcd.noCursor();
-  always_on_display();
-  
+  CheckAllButtons();
+ 
+ 
 }
 
-void greeting(){
-  lcd.setCursor(0,0);
-  lcd.print("Konichiwaa");
-  
-  lcd.createChar(0, Heart);
-  lcd.setCursor(11,0);
-  lcd.write(byte(0));
-  delay(2000);
-  lcd.clear(); 
-  
-  if(long_start){
-    lcd.setCursor(2,0);
-    lcd.print("Please wait");
-    for(int i = 0; i <= 16; i++){
-      lcd.setCursor(i,1);
-      lcd.print("-");
-      delay(500);
-    }
-    lcd.clear(); 
-    lcd.setCursor(5,0);
-    lcd.print("Done");
-    delay(2000);
-    lcd.clear(); 
+
+int getAverageOutput(int pinToRead){
+  index = 0;
+  total = 0; 
+ 
+  while (index < numReadings){
+    readings[index] = analogRead(pinToRead);
+    total = total + readings[index];
+    index = index + 1;
+    //delay (1);
   }
-  lcd.setCursor(1,0);
-  lcd.print("Enjoy the ride");
-  delay(3000);
-  lcd.clear();   
+  return total / numReadings;
 }
 
-void always_on_display(){
-  stopwatch();
-  if(engine_status_flag == 1){
-    lcd.createChar(1, Lightning);
-    lcd.setCursor(0,1);
-    lcd.write(byte(1));
-  }
-}
 
-void stopwatch(){
-  if (actual_time - first_timer >= 1000UL) {
-    
-    first_timer = actual_time;
-    // stopwatch section ----------
-    if (seconds > 59){
-      minutes++;
-      seconds = 0;
-      lcd.clear();
-    }
-    if(minutes == 60){
-      hours++;
-      minutes = 0;
-      lcd.clear();
-    }
-    seconds++;
-    // ----------
-    lcd.setCursor(0,0);  // stopwatch location
-    lcd.print("Trip time: ");
-    if(hours < 10){
-      lcd.print("0");
-    }
-    lcd.print(hours);   
-    lcd.print(":");
-    if(minutes < 10){
-      lcd.print("0");
-    }
-    lcd.print(minutes);
-  }
-  
+void CheckAllButtons(void) {
+      if (buttbx.getKeys())
+    {
+       for (int i=0; i<LIST_MAX; i++)   
+        {
+           if ( buttbx.key[i].stateChanged )   
+            {
+            switch (buttbx.key[i].kstate) { 
+                    case PRESSED:
+                    case HOLD:
+                              Joystick.setButton(buttbx.key[i].kchar, 1);
+                              break;
+                    case RELEASED:
+                    case IDLE:
+                              Joystick.setButton(buttbx.key[i].kchar, 0);
+                              break;
+            }
+           }   
+         }
+     }
 }
